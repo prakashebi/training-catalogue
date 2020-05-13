b95bdabece800fbc81fb55c8fd8df7c73fb3ef92
Prakash Singh Gaur <prakash@c02t7332h03m.windows.ebi.ac.uk>
2020-05-13 12:58:58 +0530

    first build commit

---
+++ precache-manifest.3b9c671c6048a1c0787b5008db64e19d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becb587222c803021614d24041e838a8",
+    "url": "/training-catalogue/index.html"
+  },
+  {
+    "revision": "502b3f0c88cbab2c86af",
+    "url": "/training-catalogue/static/css/main.b8ab1321.chunk.css"
+  },
+  {
+    "revision": "e565a2bc54d693ed0bbd",
+    "url": "/training-catalogue/static/js/2.1b754925.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/training-catalogue/static/js/2.1b754925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502b3f0c88cbab2c86af",
+    "url": "/training-catalogue/static/js/main.4791fc47.chunk.js"
+  },
+  {
+    "revision": "4e0edd60c93dfc1c0552",
+    "url": "/training-catalogue/static/js/runtime-main.29c1e2e0.js"
+  },
+  {
+    "revision": "b17839a4832288516e00beed241bf46f",
+    "url": "/training-catalogue/static/media/cabana.b17839a4.png"
+  },
+  {
+    "revision": "c93e6d3b69970496e9f201b97eac7fc4",
+    "url": "/training-catalogue/static/media/card8.c93e6d3b.png"
+  },
+  {
+    "revision": "c77ffae57812d90b1f47ad3576b95d43",
+    "url": "/training-catalogue/static/media/corbel.c77ffae5.png"
+  },
+  {
+    "revision": "5285ccde26ef42e7ca594cc3737b637f",
+    "url": "/training-catalogue/static/media/ritrain.5285ccde.png"
+  }
+]);
